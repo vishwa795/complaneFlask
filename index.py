1fcf621eba668156826d723b628f057d6af21e1e
roy-a2yush <roy.a2yush@gmail.com>
2021-04-24 22:53:41 +0530

    routes

--- index.py
+++ index.py
@@ -1,6 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/fastText')
+def fastText():
+    #add code for fastText classification
+
+
+
+@app.route('/keyBert')
+def keyBert():
+    #add code for keyBert
+
+
+
+@app.route('/postitionRank')
+def positionRank():
+    #add code for fastText positionRank
+
+
+
+@app.route('/annoy')
+def annoy():
+    #add code for fastText classification
